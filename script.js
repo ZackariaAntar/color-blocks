552c7385bda1b9ad737ae60fc05e8991edea033d
Zackaria Antar <antarzack@gmail.com>
2023-05-04 17:23:51 -0500

    finished part 1

--- script.js
+++ script.js
@@ -4,22 +4,40 @@ console.log("🟥 🟦 🟩 🟨");
 // Additionally, when you click on a color block, it should: Remove that block from the DOM
 
 function onReady(){
-    $('#red-btn').on('click', );
-    $('#blue-btn').on('click', );
-    $('#green-btn').on('click', );
-    $('#yellow-btn').on('click', );
+    $('#red-btn').on('click',appendRed);
+    $('#blue-btn').on('click', appendBlue);
+    $('#green-btn').on('click', appendGreen);
+    $('#yellow-btn').on('click', appendYellow);
+    $('#blocks').on('click', '.red-fill', removeBlock)
+    $('#blocks').on('click', '.blue-fill', removeBlock)
+    $('#blocks').on('click', '.green-fill', removeBlock)
+    $('#blocks').on('click', '.yellow-fill', removeBlock)
 
 
 }
 //____________________________out of page load_____________________//
 
+// find out if a conditional can be initialized inside the append to conditionally append based on the button.
+
 function appendRed(){
+    $("#blocks").append('<div class="block red-fill"></div>');
+
+}
+function appendBlue(){
+    $("#blocks").append('<div class="block blue-fill"></div>');
+
+}
+function appendGreen(){
+    $("#blocks").append('<div class="block green-fill"></div>');
+
+}
+function appendYellow(){
+    $("#blocks").append('<div class="block yellow-fill"></div>');
     
-
-
 }
 
 function removeBlock(){
+    $(this).remove()
 
 }
 
