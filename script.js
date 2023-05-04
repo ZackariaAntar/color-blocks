42a610b5cea516adeb38d2e437eadb24c9e1fdb5
Zackaria Antar <antarzack@gmail.com>
2023-05-04 18:17:42 -0500

    added functionality to recount the length of the jquery array of blocks for when the blocks are removed from the DOM in order to have accurate count of blocks.

--- script.js
+++ script.js
@@ -3,52 +3,65 @@ console.log("🟥 🟦 🟩 🟨");
 // When you click on a button, it should: Add a color block of that color to the DOM.
 // Additionally, when you click on a color block, it should: Remove that block from the DOM
 
-function onReady(){
-    $('#red-btn').on('click',appendRed);
-    $('#blue-btn').on('click', appendBlue);
-    $('#green-btn').on('click', appendGreen);
-    $('#yellow-btn').on('click', appendYellow);
-    // $('#blocks').on('click', '.red-fill', removeBlock)
-    // $('#blocks').on('click', '.blue-fill', removeBlock)
-    // $('#blocks').on('click', '.green-fill', removeBlock)
-    // $('#blocks').on('click', '.yellow-fill', removeBlock)
-    // $('#blocks').on('click', '.block', removeBlock)
-
-
+function onReady() {
+	$("#red-btn").on("click", appendRed);
+	$("#blue-btn").on("click", appendBlue);
+	$("#green-btn").on("click", appendGreen);
+	$("#yellow-btn").on("click", appendYellow);
+	// $('#blocks').on('click', '.red-fill', removeBlock)
+	// $('#blocks').on('click', '.blue-fill', removeBlock)
+	// $('#blocks').on('click', '.green-fill', removeBlock)
+	// $('#blocks').on('click', '.yellow-fill', removeBlock)
+	$("#blocks").on("click", ".block", removeBlock);
 }
 //____________________________out of page load_____________________//
 
 // find out if a conditional can be initialized inside the append to conditionally append based on the button.
 
-function appendRed(){
-    $('#blocks').append('<div class="block red-fill"></div>');
-    $('#red-count').empty()
-    $('#red-count').append( $('.red-fill').length)
-
+function appendRed() {
+	$("#blocks").append('<div class="block red-fill"></div>');
+	$("#red-count").empty();
+	$("#red-count").append($(".red-fill").length);
 }
-function appendBlue(){
-    $("#blocks").append('<div class="block blue-fill"></div>');
-    $("#blue-count").empty();
+function appendBlue() {
+	$("#blocks").append('<div class="block blue-fill"></div>');
+	$("#blue-count").empty();
 	$("#blue-count").append($(".blue-fill").length);
-
 }
-function appendGreen(){
-    $("#blocks").append('<div class="block green-fill"></div>');
-    $("#green-count").empty();
+function appendGreen() {
+	$("#blocks").append('<div class="block green-fill"></div>');
+	$("#green-count").empty();
 	$("#green-count").append($(".green-fill").length);
-
 }
-function appendYellow(){
-    $("#blocks").append('<div class="block yellow-fill"></div>');
-    $("#yellow-count").empty();
+function appendYellow() {
+	$("#blocks").append('<div class="block yellow-fill"></div>');
+	$("#yellow-count").empty();
 	$("#yellow-count").append($(".yellow-fill").length);
-
 }
 
+function recountRed() {
+	$("#red-count").empty();
+	$("#red-count").append($(".red-fill").length);
+}
 
-// function removeBlock(){
-//     $(this).remove()
+function recountBlue() {
+	$("#blue-count").empty();
+	$("#blue-count").append($(".blue-fill").length);
+}
+function recountGreen() {
+	$("#green-count").empty();
+	$("#green-count").append($(".green-fill").length);
+}
 
-// }
+function recountYellow() {
+	$("#yellow-count").empty();
+	$("#yellow-count").append($(".yellow-fill").length);
+}
 
-
+function removeBlock() {
+	$(this).remove();
+	recountRed();
+	recountBlue();
+	recountGreen();
+	recountYellow();
+}
