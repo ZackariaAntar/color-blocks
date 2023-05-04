87841cc54c781db81ccc508a0c257617c913464a
Zackaria Antar <antarzack@gmail.com>
2023-05-04 16:48:44 -0500

    structuring the jQuery functions.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('🟥 🟦 🟩 🟨');
+$(document).ready(onReady);
+console.log("🟥 🟦 🟩 🟨");
+// When you click on a button, it should: Add a color block of that color to the DOM.
+// Additionally, when you click on a color block, it should: Remove that block from the DOM
+
+function onReady(){
+    $('#red-btn').on('click', );
+    $('#blue-btn').on('click', );
+    $('#green-btn').on('click', );
+    $('#yellow-btn').on('click', );
+
+
+}
+//____________________________out of page load_____________________//
+
+function appendRed(){
+    
+
+
+}
+
+function removeBlock(){
+
+}
+
+
